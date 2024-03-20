55f26964fc27fa00c879fb88d194557ec3c78f5c
SatyaRajAwasth1 <satya.211733@ncit.edu.np>
2024-03-21 00:57:16 +0545

    Add sample servers with centeral logging service

---
+++ CenteralLoggingService/src/main/java/np/com/esewa/demo/CenteralLoggingService/CenteralLoggingServiceApplication.java
@@ -0,0 +1,13 @@
+package np.com.esewa.demo.CenteralLoggingService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CenteralLoggingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CenteralLoggingServiceApplication.class, args);
+	}
+
+}
---
+++ CenteralLoggingService/src/test/java/np/com/esewa/demo/CenteralLoggingService/CenteralLoggingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package np.com.esewa.demo.CenteralLoggingService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CenteralLoggingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
