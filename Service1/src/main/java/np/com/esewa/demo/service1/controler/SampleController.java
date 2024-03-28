39952b70061084aa3a3b7a14d569167e2a520bb6
SatyaRajAwasth1 <satyaraj.awasthi@esewa.com.np>
2024-03-28 13:55:20 +0545

    Ref: update configs and logs to maintain correlation ID in log

--- Service1/src/main/java/np/com/esewa/demo/service1/controler/SampleController.java
+++ Service1/src/main/java/np/com/esewa/demo/service1/controler/SampleController.java
@@ -1,1 +1,33 @@
+package np.com.esewa.demo.service1.controler;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController()
+public class SampleController {
+
+    private static final Logger logger = LoggerFactory.getLogger(SampleController.class);
+
+    @GetMapping("/api/v1/eg")
+    public ResponseEntity<String> sayHey(@RequestParam String msg){
+        logger.info("This is a msg from client: {}", msg);
+        return new ResponseEntity<>( "Hey! I am Ok.", HttpStatus.OK);
+    }
+
+    @GetMapping()
+    public String index() {
+        logger.trace("A TRACE Message");
+        logger.debug("A DEBUG Message");
+        logger.info("An INFO Message");
+        logger.warn("A WARN Message");
+        logger.error("An ERROR Message");
+
+        return "Howdy! Check out the Logs to see the output...";
+    }
+
+}
---
+++ Service1/src/main/java/np/com/esewa/demo/service1/service/SampleService.java
@@ -0,0 +1,4 @@
+package np.com.esewa.demo.service1.service;
+
+public interface SampleService {
+}
